dfb28acbb215c5679da8b63c6c113afd2150b39a
Steve Bowman <hullcitysteve@gmail.com>
2015-02-19 23:08:25 +0000

    Added example of a simple call to a lambda function.

--- AWSLambdaSMS/SMSProcessor/InvokeLambda.aspx.cs
+++ AWSLambdaSMS/SMSProcessor/InvokeLambda.aspx.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Amazon;
+using Amazon.Lambda;
+using System;
+using Amazon.Lambda.Model;
 
 namespace SMSProcessor
 {
+    /// <summary>
+    /// Invokation of a simple lambda funcion.
+    /// </summary>
     public partial class InvokeLambda : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("Hello World");
+            // Create the client to be used to invoke the lambda function.
+            // This will use the default credentials from the profile store.
+            var config = new AmazonLambdaConfig { RegionEndpoint = RegionEndpoint.EUWest1 };
+            IAmazonLambda client = AWSClientFactory.CreateAmazonLambdaClient(config);
+
+            // Arguments for the lambda function
+            const string arguments = @"{""key1"": ""argument 1"", 
+                                        ""key2"": ""argument 2"",
+                                        ""key3"": ""argument 3""
+                                       }";
+
+            // Invoke the lambda function
+            InvokeAsyncResponse response = client.InvokeAsync("HelloWorld", arguments);
+
+            Response.Write("Completed with response code: " + response.HttpStatusCode);
         }
     }
 }
