0d1bfe2a38d7f930e8e67de90fb3589b22ba9c02
Steve Bowman <hullcitysteve@gmail.com>
2015-02-19 21:36:19 +0000

    Added empty web app to process incoming SMS messages.

---
+++ AWSLambdaSMS/SMSProcessor/InvokeLambda.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SMSProcessor
+{
+    public partial class InvokeLambda : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hello World");
+        }
+    }
+}
